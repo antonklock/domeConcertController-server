2a7ca88c1779f533b14431130b435e94789f3487
Anton Klock <klock.anton@gmail.com>
2023-12-27 15:51:27 +0100

    updating to receive users and position from client

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ const http = require('http');
 const cors = require('cors');
 const { Server } = require('socket.io');
 const express = require('express');
+const { v4: uuid } = require('uuid');
+
+const origin = process.env.NODE_ENV === 'production' ? 'https://dome-concert-controller.vercel.app' : 'http://localhost:3000';
 
 const corsOptions = {
-    origin: "https://dome-concert-controller.vercel.app",
+    origin: origin,
     methods: ["GET", "POST"],
     credentials: true,
 };
@@ -21,20 +24,31 @@ const io = new Server(server, {cors: corsOptions});
 io.on('connection', (socket) => {
     console.log('A user connected');
 
-    socket.on('updatePlayerPositions', (data) => {
+    socket.on('addPlayer', (data) => {
+        const { name, color, id } = data;
+        addPlayer(name, color, id);
+        console.log('Player added');
+    });
+
+    socket.on('removePlayer', (data) => {
+        const { id } = data;
+        removePlayer(id);
+        console.log('Player removed');
+    });
+
+    socket.on('getAllRemotePlayers', (data) => {
+        io.emit('updateAllRemotePlayers', getAllPlayers());
+    });
+
+    socket.on('updateLocalPlayer', (data) => {
         const { id, position } = data;
         updatePlayerPosition(id, position);
-        io.emit('updatePlayerPositions', getAllPlayers());
     });
 
     socket.on('disconnect', () => {
         console.log('A user disconnected');
     });
 });
-
-// app.get('/test-cors', function(req, res) {
-//     res.json({ message: 'CORS-enabled response!' });
-// });
 
 app.get('/', (req, res) => {
     res.send("Server is running...");
@@ -45,34 +59,45 @@ server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
 
-// const addPlayer = (name, color) => {
-//     players.push({
-//         id: uuid(),
-//         name,
-//         position: {
-//             x: 0,
-//             y: 0
-//         },
-//         color,
-//     });
-// }
+//TODO: Rewrite so the server hands out an ID to the client
+const addPlayer = (name, color, id) => {
+    players.push({
+        id: id,
+        name,
+        position: {
+            x: 0,
+            y: 0
+        },
+        color,
+    });
+    // console.log('Player added');
+    // console.log('Players: ');
+    // players.map(player => console.log(player.name));
+}
 
-// const removePlayer = (id) => {
-//     players = players.filter(player => player.id !== id);
-// }
-
-// const getPlayerPosition = (id) => {
-//     return players.find(player => player.id === id).position;
-// };
+const removePlayer = (id) => {
+    if (players.length > 0) {
+        players = players.filter(player => player.id !== id);    
+    } else {
+        console.log('No players to remove');
+    }
+}
 
 const getAllPlayers = () => {
     return players;
 };
 
-// const updatePlayerPosition = (id, position) => {
-//     const player = players.find(player => player.id === id);
-//     player.position = position;
-// }
+const updatePlayerPosition = (id, position) => {
+    if (players.length > 0) {
+        const player = players.find(player => player.id === id);
+        if (player) {
+            player.position = { x: position.x, y: position.y + 40 };    
+            // console.log("Updating player with id: " + id);
+        } else {
+            // console.log('No player found');
+        }
+    };
+}
 
 // const movePlayers = () => {
 //     players.forEach(player => {
@@ -89,7 +114,7 @@ const getAllPlayers = () => {
 //     setInterval(() => {
 //         movePlayers();
 //         io.emit('updatePlayerPositions', getAllPlayers());
-//     }, 16.67);
+//     }, 33);
 // }
 
 // moveAllPlayers();
